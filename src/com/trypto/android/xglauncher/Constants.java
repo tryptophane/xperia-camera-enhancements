f8d5f895ba3b179f452a2d310b034d44a6678c27
tryptophane <pascal.pollet@mailbox.org>
2015-11-21 16:00:04 +0100

    added more camera modes + refactorings

--- src/com/trypto/android/xglauncher/Constants.java
+++ src/com/trypto/android/xglauncher/Constants.java
@@ -1,7 +1,15 @@
 package com.trypto.android.xglauncher;
 
 public class Constants {
-	public static final String ALBUM_LAUNCHER_PATH = "com.sonyericsson.cameracommon.launcher.AlbumLauncher";
-	public static final String APP_PACKAGE = "com.sonyericsson.android.camera";
+	public static final String APP_PACKAGE_CAMERA = "com.sonyericsson.android.camera";
+	public static final String APP_PACKAGE_AR_EFFECT="com.sonymobile.androidapp.cameraaddon.areffect";
+	public static final String APP_PACKAGE_BACKGROUND_DEFOCUS="com.sonymobile.backgrounddefocus";
+	public static final String APP_PACKAGE_CAMERA_3D = "com.sonyericsson.android.camera3d";
+	public static final String APP_PACKAGE_ART_CAMERA = "com.sonyericsson.android.addoncamera.artfilter";
+
+	public static final String PATH_ALBUM_LAUNCHER = "com.sonyericsson.cameracommon.launcher.AlbumLauncher";
+	public static final String PATH_AR_EFFECT_MAIN_UI="com.sonymobile.androidapp.cameraaddon.areffect.MainUi";
+	public static final String PATH_ANDROID_APP_ACTIVITY = "android.app.Activity";
+
 	public static final String TIMESHIFT_IDENT = "DCIM/XPERIA/TIMESHIFT";
 }
--- src/com/trypto/android/xglauncher/Xglauncher.java
+++ src/com/trypto/android/xglauncher/Xglauncher.java
@@ -1,8 +1,15 @@
 package com.trypto.android.xglauncher;
 
-import static com.trypto.android.xglauncher.Constants.ALBUM_LAUNCHER_PATH;
-import static com.trypto.android.xglauncher.Constants.APP_PACKAGE;
+import static com.trypto.android.xglauncher.Constants.APP_PACKAGE_ART_CAMERA;
+import static com.trypto.android.xglauncher.Constants.APP_PACKAGE_AR_EFFECT;
+import static com.trypto.android.xglauncher.Constants.APP_PACKAGE_BACKGROUND_DEFOCUS;
+import static com.trypto.android.xglauncher.Constants.APP_PACKAGE_CAMERA;
+import static com.trypto.android.xglauncher.Constants.APP_PACKAGE_CAMERA_3D;
+import static com.trypto.android.xglauncher.Constants.PATH_ALBUM_LAUNCHER;
+import static com.trypto.android.xglauncher.Constants.PATH_ANDROID_APP_ACTIVITY;
+import static com.trypto.android.xglauncher.Constants.PATH_AR_EFFECT_MAIN_UI;
 import static com.trypto.android.xglauncher.Constants.TIMESHIFT_IDENT;
+import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
@@ -11,53 +18,113 @@ import java.io.File;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodHook.MethodHookParam;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class Xglauncher implements IXposedHookLoadPackage {
 
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-		if (!lpparam.packageName.equals(APP_PACKAGE))
-			return;
+		if (lpparam.packageName.equals(APP_PACKAGE_CAMERA) || lpparam.packageName.equals(APP_PACKAGE_CAMERA_3D)
+				|| lpparam.packageName.equals(APP_PACKAGE_ART_CAMERA)) {
+			final Class<?> AlbumLauncher = findClass(PATH_ALBUM_LAUNCHER, lpparam.classLoader);
 
-		final Class<?> AlbumLauncher = findClass(ALBUM_LAUNCHER_PATH, lpparam.classLoader);
+			findAndHookMethod(PATH_ALBUM_LAUNCHER, lpparam.classLoader, "launchAlbum", Activity.class, Uri.class,
+					String.class, int.class, boolean.class, new XC_MethodHook() {
+						@Override
+						protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 
-		findAndHookMethod(ALBUM_LAUNCHER_PATH, lpparam.classLoader, "launchAlbum", Activity.class, Uri.class,
-				String.class, int.class, boolean.class, new XC_MethodHook() {
-					@Override
-					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+							final Activity activity = (Activity) param.args[0];
+							final Uri uri = (Uri) param.args[1];
+							final String s = (String) param.args[2];
+							final boolean b = (Boolean) param.args[4];
 
-						final Activity activity = (Activity) param.args[0];
-						final Uri uri = (Uri) param.args[1];
-						final String s = (String) param.args[2];
-						final boolean b = (Boolean) param.args[4];
+							if (needsSonyGallery(activity, uri, s, b)) {
+								return;
+							}
 
-						final String realPath = getRealPathFromURI(activity, uri);
-						XposedBridge.log("Xglauncher: Path to medium: " + realPath);
-
-						boolean mpoPresent = false;
-
-						if (realPath.endsWith(".JPG") && new File(realPath.replaceAll(".JPG$", ".MPO")).exists()) {
-							mpoPresent = true;
+							XposedBridge.log("Xglauncher: Starting user gallery...");
+							callStaticMethod(AlbumLauncher, "launchPlayer", activity, uri, s);
+							param.setResult(null);
 						}
+					});
+		} else if (lpparam.packageName.equals(APP_PACKAGE_AR_EFFECT)) {
+			findAndHookMethod(PATH_AR_EFFECT_MAIN_UI, lpparam.classLoader, "launchAlbum", Uri.class,
+					new XC_MethodHook() {
+						@Override
+						protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+							XposedBridge.log("Xglauncher: Hooking Method launchAlbum in MainUi of AR Effect addon...");
 
-						if (MimeType.fromText(s) == MimeType.MPO || b || realPath.contains(TIMESHIFT_IDENT)
-								|| mpoPresent) {
-							XposedBridge.log("Xglauncher: Fallback to original method");
-							return;
+							Uri uri = (Uri) param.args[0];
+							Activity activity = (Activity) callMethod(param.thisObject, "getActivity");
+
+							if (needsSonyGallery(activity, uri, "", false)) {
+								return;
+							}
+
+							startUserGallery(uri, param, activity);
 						}
+					});
+		} else if (lpparam.packageName.equals(APP_PACKAGE_BACKGROUND_DEFOCUS)) {
+			findAndHookMethod(PATH_ANDROID_APP_ACTIVITY, lpparam.classLoader, "startActivityForResult", Intent.class,
+					int.class, new XC_MethodHook() {
+						@Override
+						protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+							XposedBridge.log(
+									"Xglauncher: Hooking Method startActivityForResult in superclass Activity from ViewFinderActivity of Background Defocus addon...");
+							Intent intent = (Intent) param.args[0];
 
-						XposedBridge.log("Xglauncher: Start user gallery...");
-						callStaticMethod(AlbumLauncher, "launchPlayer", activity, uri, s);
-						param.setResult(null);
+							if (intent.getAction().equals("android.intent.action.VIEW")
+									|| !intent.getPackage().equals("com.sonyericsson.album")) {
+								return;
+							}
 
-					}
-				});
+							Uri uri = intent.getData();
+							if (needsSonyGallery((Activity) param.thisObject, uri, "", false)) {
+								return;
+							}
+
+							startUserGallery(uri, param, (Activity) param.thisObject);
+							param.setResult(null);
+						}
+					});
+		}
+	}
+
+	private void startUserGallery(Uri uri, MethodHookParam param, Activity activity) {
+		final Intent intent = new Intent("android.intent.action.VIEW");
+		intent.addCategory("android.intent.category.DEFAULT");
+		intent.putExtra("android.intent.extra.finishOnCompletion", true);
+		intent.setDataAndType(uri, MimeType.PHOTO.getText());
+		if (intent.resolveActivity(activity.getPackageManager()) != null) {
+			XposedBridge.log("Xglauncher: Starting user gallery...");
+			activity.startActivity(intent);
+			param.setResult(null);
+		}
+	}
+
+	private boolean needsSonyGallery(final Activity activity, final Uri uri, final String s, final boolean b) {
+		final String realPath = getRealPathFromURI(activity, uri);
+		XposedBridge.log("Xglauncher: Path to medium: " + realPath);
+
+		boolean mpoPresent = false;
+
+		if (realPath.toUpperCase().endsWith(".JPG") && (new File(realPath.replaceAll("(?i)\\.JPG$", ".MPO")).exists()
+				|| new File(realPath.replaceAll("(?i)\\.JPG$", ".mpo")).exists())) {
+			mpoPresent = true;
+		}
+
+		if (MimeType.fromText(s) == MimeType.MPO || b || realPath.contains(TIMESHIFT_IDENT) || mpoPresent) {
+			XposedBridge.log("Xglauncher: Fallback to original method");
+			return true;
+		}
+		return false;
 	}
 
 	public String getRealPathFromURI(Context context, Uri contentUri) {
@@ -93,6 +160,10 @@ public class Xglauncher implements IXposedHookLoadPackage {
 				}
 			}
 			return MimeType.UNKOWN;
+		}
+
+		public String getText() {
+			return mText;
 		}
 	}
 
